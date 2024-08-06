40ab4c822161ad1c632d5fac1bc5a58e06b38abd
Vincent Hoang <vincent.hoang@posteo.net>
2024-08-06 12:29:32 +0700

    0.1.13: string::to_vec

--- src/string.rs
+++ src/string.rs
@@ -1,6 +1,6 @@
 pub fn split<P: AsRef<str>>(data: &str, index: usize, pat: P) -> Option<String> {
-    let parts: Vec<&str> = data.split(pat.as_ref()).collect();
-    parts.get(index).map(|&s| s.to_string())
+    let parts: Vec<String> = to_vec(data, pat);
+    parts.get(index).map(|s|s.to_owned())
 }
 
 pub fn split_to<T: std::str::FromStr, P: AsRef<str>>(data: &str, index: usize, pat: P) -> Option<T> {
@@ -9,4 +9,8 @@ pub fn split_to<T: std::str::FromStr, P: AsRef<str>>(data: &str, index: usize, p
     } else {
         None
     }
+}
+
+pub fn to_vec<P: AsRef<str>>(data: &str, pat: P) -> Vec<String> {
+    data.split(pat.as_ref()).collect::<Vec<&str>>().into_iter().map(|s| s.to_string()).collect::<Vec<String>>()
 }
